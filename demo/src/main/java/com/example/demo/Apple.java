bd28c72a01d9420b46a832da19c07662b3834df2
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-09-23 11:35:31 +0530

    Bean Practical

---
+++ demo/src/main/java/com/example/demo/Apple.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+public class Apple {
+
+    void eatApple(){
+        System.out.println("I am eating apple");
+    }
+}
